11688d6c168733b426c4763e01e0193f3bcd1aa7
deepakshrma <deepak.m.shrma@gmail.com>
2016-03-04 02:37:15 +0530

    Completed script

--- init.sh
+++ init.sh
@@ -6,7 +6,12 @@ APP_NAME=$1
 fi
 mkdir $APP_NAME
 mkdir $APP_NAME/app
-
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp $SCRIPT_DIR/{index.html,package.json,tsconfig.json,typings.json} $APP_NAME/
+cp $SCRIPT_DIR/app/* $APP_NAME/app/
+echo "Installing node package, It will take time"
+sleep 3
 npm i
-cd $APP_NAME
-
+echo "Starting your app, see logs: Thanks :-)"
+sleep 3
+npm start
